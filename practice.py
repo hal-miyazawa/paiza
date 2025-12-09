748ee73973b5b6fb90724d2d91a0dabc05d2bff7
hal-miyazawa <zawato1026@gmail.com>
2025-12-10 00:06:32 +0900

    comit

---
+++ practice.py
@@ -0,0 +1,36 @@
+# while文
+# while i < n:
+#     --
+    # i += 1
+
+# for 文
+# for i in range(n):
+#     --
+
+# list
+# numbers = [1, 3, 5, 6, 3, 2, 5, 23, 2]
+
+# total = 0
+
+# for value in numbers:
+#     total = total + value
+
+# print(total)
+
+# if
+# if n >= 5:
+#     --
+# else:
+#     --
+
+# リスト内で５以上の数の合計
+# numbers = [4,0,5,-1,3,10,6,-8]
+# total = 0
+# for number in numbers:
+#     if number >= 5:
+#         total += number
+# print(total)
+
+
+# https://paiza.jp/works/mondai/prob60/problem_index?language_uid=python3
+# 文字数を切り取るから
